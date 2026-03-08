0eba65dd778aa0faadd14640e8386870fa7552ee
ldfd <ldfd@outlook.it>
2026-03-08 11:37:24 +0800

    init

---
+++ seckill/src/test/java/com/ldfd/seckill/SeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldfd.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
